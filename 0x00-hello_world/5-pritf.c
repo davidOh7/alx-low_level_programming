7bba2634894d7877395c22d151c4745277362534
David Yakubu <yakubudav46@hotmail.com>
2023-02-24 01:34:43 -0800

    Printf function

---
+++ 0x00-hello_world/5-pritf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A progragmming for printing on C
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
